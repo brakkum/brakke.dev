ececfb2f25f3fea68f0a869429eeabc7deceb1d2
Daniel Brakke <brakscript@gmail.com>
2020-11-26 10:53:26 -0600

    strip out stuff from page

--- index.php
+++ index.php
@@ -8,23 +8,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
         <style>
-            body p {
-                margin-bottom: .8em;
-            }
-            a, a:hover {
-                color: rgb(142, 173, 254);
-            }
-            .columns .column {
-                padding: 0 .75rem;
-            }
-            .column ul {
-                margin-top: 0;
-            }
-            h3.projects-title {
-                margin-bottom: 2rem;
-            }
-            .columns.project-columns {
-                margin-top: 0;
+            .logo {
+                width: 50px;
+                height: 50px;
             }
         </style>
     </head>
@@ -34,72 +20,8 @@
                 Dan Brakke
             </h1>
             <h2 class="is-size-3 has-text-grey-lighter">Developer - <a href="https://fjorgedigital.com" target="_blank" style="color: rgb(254,118,52);">fjorge</a></h2>
-            <p>
-                I am a developer that hails from the Twin Cities.
-                I spent much of my adult life as a musician and photographer, only to find my true calling as a developer while attending Dunwoody College of Technology.
-            </p>
-            <p>
-                I like to spend my free time learning as much about technology as possible, and keeping up to date with the current trends in the industry.
-            </p>
-            <h3 class="is-size-3 has-text-grey-lighter projects-title">Personal Projects</h3>
-            <div class="columns project-columns">
-                <div class="column">
-                    <ul>
-                        <li>
-                            <a href="https://github.com/brakkum/ZOoTR-Entrance-Tracker" target="_blank">Ocarina of Time Entrance Tracker</a>
-                            <ul>
-                                <li>JavaScript - React</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="https://github.com/brakkum/MyGig" target="_blank">MyGig</a>
-                            <ul>
-                                <li>C# - .NET Core, Entity Framework</li>
-                                <li>JavaScript - React</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <div class="column">
-                    <ul>
-                        <li>
-                            <a href="https://github.com/brakkum/linkst.rip" target="_blank">linkst.rip</a>
-                            <ul>
-                                <li>PHP - Symfony</li>
-                                <li>JavaScript - React</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="https://github.com/brakkum/secure-file-transfer" target="_blank">fileshare.brakke.dev</a>
-                            <ul>
-                                <li>PHP - Symfony</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <div class="column">
-                    <ul>
-                        <li>
-                            <a href="https://github.com/brakkum/photos.brakke.dev" target="_blank">photos.brakke.dev</a>
-                            <ul>
-                                <li>Javascript - React, Node</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="https://github.com/brakkum/timelapse_eq" target="_blank">Timelapse_EQ</a>
-                            <ul>
-                                <li>Python</li>
-                            </ul>
-                        </li>
-                        <li class="project-list-item">
-                            <a href="https://github.com/brakkum/gigs.brakke.dev" target="_blank">gigs.brakke.dev</a>
-                            <ul>
-                                <li>PHP</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </section>
+            <p>I write code.</p>
+            <br/>
+            <a class="logo" href="https://github.com/brakkum"><img class="logo" alt="GitHub Logo" src="github.png" /></a>
     </body>
 </html>
